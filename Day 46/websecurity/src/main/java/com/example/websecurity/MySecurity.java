fda36be17c8c86b33bc133efb14f7992f811a64d
kalyaniNalawade <105098749+kalyaniNalawade@users.noreply.github.com>
2022-05-30 14:28:19 +0530

    initial commit

---
+++ Day 46/websecurity/src/main/java/com/example/websecurity/MySecurity.java
@@ -0,0 +1,32 @@
+package com.example.websecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class MySecurity extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("abc")
+		.password("xyz")
+		.roles("USER")
+	    .and()
+		.withUser("kalyani")
+		.password("kalyani")
+		.roles("admin");
+		
+		
+	}
+@Bean
+public PasswordEncoder getPasswordEncoder() {
+	return NoOpPasswordEncoder.getInstance();
+	
+	
+}
+}
